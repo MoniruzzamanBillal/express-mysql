a3637c7757b66e9bbaca32464f258039627ff95c
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-04-14 22:19:20 +0600

    login method added

--- server/controller/StudentController.js
+++ server/controller/StudentController.js
@@ -1,4 +1,5 @@
 const database = require("../Database.js");
+const jwt = require("jsonwebtoken");
 
 //! functionality for getting student info
 module.exports.getStudents = async (req, res) => {
@@ -12,6 +13,58 @@ module.exports.getStudents = async (req, res) => {
     });
   } catch (error) {
     console.log("error in get students method!!");
+    res.send({ error });
+  }
+};
+
+//! functionality for creating new student
+module.exports.createStudent = async (req, res) => {
+  try {
+    const data = req.body;
+    const values = Object.values(data);
+
+    const que = `insert into student values(?)`;
+
+    database.query(que, [values], (error, data) => {
+      if (error) {
+        return res.send({ error });
+      }
+      return res.send({ message: "student created successfully", data });
+    });
+  } catch (error) {
+    console.log("error in create students method!!");
+    res.send({ error });
+  }
+};
+
+//! function for login a user
+module.exports.login = async (req, res) => {
+  try {
+    const que = `select * from student where userEmail=? && userPassword=? `;
+    const { userEmail, userPassword } = req.body;
+
+    database.query(que, [userEmail, userPassword], (error, data) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      if (data?.length < 1) {
+        return res.send({ data: "User dont exist!! " });
+      }
+
+      const token = jwt.sign(
+        {
+          userEmail: data[0]?.userEmail,
+          userPassword: data[0]?.userPassword,
+        },
+        "secretKey",
+        { expiresIn: "6h" }
+      );
+
+      return res.send({ token, message: "Login successfully !! " });
+    });
+  } catch (error) {
+    console.log("error in login method!!");
     res.send({ error });
   }
 };
--- server/routes/Student.js
+++ server/routes/Student.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getStudents } = require("../controller/StudentController");
+const {
+  getStudents,
+  createStudent,
+  login,
+} = require("../controller/StudentController");
 
 router.get("/students", getStudents);
+router.post("/addStudent", createStudent);
+router.post("/login", login);
 
 module.exports = router;
